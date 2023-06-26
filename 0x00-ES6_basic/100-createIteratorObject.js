85d69abc5f88eb360d95ff9a3b8c73f7f37f90c2
wegulohillary <wegulohillary@gmail.com>
2023-06-26 11:20:02 -0700

    corrections

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,30 +1,7 @@
 export default function createIteratorObject(report) {
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
-  const departments = Object.values(report.allEmployees);
-  
-  return {
-    next() {
-      if (currentDepartmentIndex >= departments.length) {
-        return {
-          done: true
-        };
-      }
-      
-      const employees = departments[currentDepartmentIndex];
-      const employee = employees[currentEmployeeIndex];
-      
-      if (currentEmployeeIndex < employees.length - 1) {
-        currentEmployeeIndex++;
-      } else {
-        currentDepartmentIndex++;
-        currentEmployeeIndex = 0;
-      }
-      
-      return {
-        value: employee,
-        done: false
-      };
-    }
-  };
+  const nArr = [];
+  for (const arr of Object.values(report)) {
+    nArr.push(arr);
+  }
+  return nArr;
 }
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = '';
-  const iterator = reportWithIterator.next();
-  
-  while (!iterator.done) {
-    result += iterator.value + ' | ';
-    iterator = reportWithIterator.next();
-  }
-  
-  // Remove the trailing ' | ' from the result
-  result = result.slice(0, -3);
-  
-  return result;
+  return reportWithIterator.join(' | ');
 }
